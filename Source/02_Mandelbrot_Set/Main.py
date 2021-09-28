c7ec69b109679be580e85edbc3830e8fb64642ce
GZhonghui <mail@gzher.com>
2021-09-28 17:02:41 +0800

    Mandelbrot Set

--- Source/02_Mandelbrot_Set/Main.py
+++ Source/02_Mandelbrot_Set/Main.py
@@ -1,6 +1,4 @@
 import taichi as ti
-import cv2 as cv
-from taichi.lang.ops import mod
 
 ti.init(arch=ti.gpu)
 
@@ -12,7 +10,8 @@ pixels_u8 = ti.Vector.field(3, dtype=ti.u8, shape=(n, n))
 def convert_pixel():
     for i,j in pixels:
         for k in ti.static(range(3)):
-            pixels_u8[i,j][2-k]=ti.cast(pixels[i,j][k] * 255.999, ti.u8)
+            # OpenCV and Taichi are different
+            pixels_u8[i,j][2-k]=ti.cast(pixels[j,i][k] * 255.999, ti.u8)
 
 @ti.func
 def complex_sqr(z):
@@ -23,45 +22,35 @@ def color(x):
     return ti.Vector([x, 1 - abs(x - 0.5), 1 - x])
 
 @ti.kernel
-def paint(t: ti.f32):
+def paint():
     for i, j in pixels:
-        # Euler's formula: e^{i*x} = cos(x) + sin(x)*i
-        # c = 0.7885 * e^{i*a}
-        c = ti.Vector([ti.cos(t), ti.sin(t)]) * 0.7885
-        z = ti.Vector([i / n - 0.5, j / n - 0.5]) * 3
+        z = ti.Vector([0.0, 0.0], dt=ti.f32)
+        c = ti.Vector([i / n - 0.75, j / n - 0.5], dt=ti.f32) * 3
         iterations = 0
         while z.norm() < 20 and iterations < 50:
             z = complex_sqr(z) + c
             iterations += 1
         pixels[i, j] = color(1 - iterations * 0.02)
 
-gui = ti.GUI("Julia Set", res=(n, n))
-
-video_fps = 30
-video_file_name = 'Encode.mp4'
-video_encoder = cv.VideoWriter(video_file_name, \
-    cv.VideoWriter_fourcc(*'mp4v'), \
-    video_fps, (n, n))
+gui = ti.GUI("Mandelbrot Set", res=(n, n))
 
 def main():
     try:
-        time_step = 0        
-        save_rate = 3
-        render_rate = 0.01
         while True:
-            paint(time_step * render_rate)
+            paint()
             gui.set_image(pixels)
             gui.show()
-
-            time_step += 1
-
-            if not mod(time_step, save_rate):
-                convert_pixel()
-                video_encoder.write(pixels_u8.to_numpy())
             
     except RuntimeError:
         pass
-    video_encoder.release()
+
+    try:
+        import cv2 as cv
+    except:
+        pass
+    else:
+        convert_pixel()
+        cv.imwrite('Mandelbrot Set.png', pixels_u8.to_numpy())
 
 if __name__=='__main__':
     main()
--- readMe.md
+++ readMe.md
@@ -5,13 +5,19 @@
 > Julia Set Animation, Colored. Ref: [Wikipedia](https://en.wikipedia.org/wiki/Julia_set)  
 > c=0.7885\*exp(i\*t)
 ```
->> cd Source\01_Julia_Set
+>> cd Source/01_Julia_Set
 >> python Main.py
 ```
 ![Julia_Set](readMe/01_Julia_Set_01.gif)
 
 ## 02 Mandelbrot Set
-> [Taichi] version 0.7.20, llvm 10.0.0, commit 284f75ed, win, python 3.7.9
+> [Taichi] version 0.7.20, llvm 10.0.0, commit 284f75ed, win, python 3.7.9  
+> Mandelbrot Set
+```
+>> cd Source/02_Mandelbrot_Set
+>> python Main.py
+```
+![Mandelbrot_Set](readMe/02_Mandelbrot_Set_01.png)
 
 ## 03 Export Mesh
 
@@ -19,9 +25,9 @@
 > [Taichi] version 0.7.20, llvm 10.0.0, commit 284f75ed, win, python 3.7.9  
 > Encode Frames to Video, relay on opencv-python
 ```
->> cd Source\04_Export_Video
+>> cd Source/04_Export_Video
 >> python Main.py
-Then you got a mp4 file, you can convert it to gif in Photoshop
+Then you got a mp4 file, you can convert it to **GIF** in Photoshop
 ```
 
 ## 05 GUI
