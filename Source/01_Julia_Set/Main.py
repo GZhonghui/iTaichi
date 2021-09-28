dc8885c491b2bdf9152da02b6518db7a748bc5bf
GZhonghui <mail@gzher.com>
2021-09-28 17:09:56 +0800

    Julia Set

--- Source/01_Julia_Set/Main.py
+++ Source/01_Julia_Set/Main.py
@@ -1,18 +1,9 @@
 import taichi as ti
-import cv2 as cv
-from taichi.lang.ops import mod
 
 ti.init(arch=ti.gpu)
 
 n = 640
 pixels = ti.Vector.field(3, dtype=ti.f32, shape=(n, n))
-pixels_u8 = ti.Vector.field(3, dtype=ti.u8, shape=(n, n))
-
-@ti.kernel
-def convert_pixel():
-    for i,j in pixels:
-        for k in ti.static(range(3)):
-            pixels_u8[i,j][2-k]=ti.cast(pixels[i,j][k] * 255.999, ti.u8)
 
 @ti.func
 def complex_sqr(z):
@@ -37,18 +28,9 @@ def paint(t: ti.f32):
 
 gui = ti.GUI("Julia Set", res=(n, n))
 
-'''
-video_fps = 30
-video_file_name = 'Encode.mp4'
-video_encoder = cv.VideoWriter(video_file_name, \
-    cv.VideoWriter_fourcc(*'mp4v'), \
-    video_fps, (n, n))
-'''
-
 def main():
     try:
-        time_step = 0        
-        save_rate = 3
+        time_step = 0
         render_rate = 0.01
         while True:
             paint(time_step * render_rate)
@@ -56,16 +38,9 @@ def main():
             gui.show()
 
             time_step += 1
-
-            '''
-            if not mod(time_step, save_rate):
-                convert_pixel()
-                video_encoder.write(pixels_u8.to_numpy())
-            '''
             
     except RuntimeError:
         pass
-    # video_encoder.release()
 
 if __name__=='__main__':
     main()
